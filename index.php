26cb5f0e78c7a77ccd533d21001ab44f3e7c8c01
bertrand214 <zoubrouska@hotmail.com>
2016-01-13 11:15:19 +0100

    accueil

--- index.php
+++ index.php
@@ -2,9 +2,67 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css" />
     <title>Accueil</title>
 </head>
 <body>
-    <h1>Accueil</h1>
+	<div class= "contenair">
+		<div class= "avatar">
+			<p>Prénom NOM</p>
+			<p> Tél :</p>
+			<p> Email:</p> 
+		</div>
+			<nav>
+				<ul> 
+					<li><a href="#ACCEUIL">ACCEUIL</a></li>
+					<li><a href="#NEWS">les news</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</nav>	
+	</div>
+
+<main>
+	<h1>Titre Porfolio</h1>
+
+	<div id="slider">
+		<img src="#" alt="">
+	</div>
+
+	<section id="news">
+		<h2>Les News</h2>
+		<div class="article">
+			<h3> Nouvelle actu </h3>
+			<p>comme en 2013, David Bowie n’a laissé à personne d’autre le soin de lancer la rentrée musicale pop rock.
+			Mais si Blackstar fait plus que jamais événement à l'aune de la disparisiton du chanteur, 
+			il n’éclipse pas pour autant la conséquente vague de sorties d’albums majeurs ou attendus
+			d’ici le printemps, passée une première fournée importante le 22 janvier (Fat White Family, Tortoise, Ty Segall, Tindersticks,
+			Dionysos, Savages, Tricky, Suede...). Tour d’horizon non exhaustif des productions marquantes à surveiller…</p>
+			</p>  
+		</div>
+
+		<div class="article">
+			<h3> Nouvelle actu </h3>
+			<p>comme en 2013, David Bowie n’a laissé à personne d’autre le soin de lancer la rentrée musicale pop rock.
+			Mais si Blackstar fait plus que jamais événement à l'aune de la disparisiton du chanteur, 
+			il n’éclipse pas pour autant la conséquente vague de sorties d’albums majeurs ou attendus
+			d’ici le printemps, passée une première fournée importante le 22 janvier (Fat White Family, Tortoise, Ty Segall, Tindersticks,
+			Dionysos, Savages, Tricky, Suede...). Tour d’horizon non exhaustif des productions marquantes à surveiller…</p>
+			</p>  
+		</div>
+
+
+		<div class="article">
+			<h3> Nouvelle actu </h3>
+			<p>comme en 2013, David Bowie n’a laissé à personne d’autre le soin de lancer la rentrée musicale pop rock.
+			Mais si Blackstar fait plus que jamais événement à l'aune de la disparisiton du chanteur, 
+			il n’éclipse pas pour autant la conséquente vague de sorties d’albums majeurs ou attendus
+			d’ici le printemps, passée une première fournée importante le 22 janvier (Fat White Family, Tortoise, Ty Segall, Tindersticks,
+			Dionysos, Savages, Tricky, Suede...). Tour d’horizon non exhaustif des productions marquantes à surveiller…</p>
+			</p>  
+		</div>	
+	</section>
+</main>
 </body>
+
+
 </html>
