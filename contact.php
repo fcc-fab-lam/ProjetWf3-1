e4a75f61640a38b1c7b5cb0ac7faac40a8070e9d
bertrand214 <zoubrouska@hotmail.com>
2016-01-13 12:21:26 +0100

    formulaire contact

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php include_once 'inc/header.php'; ?>
+
+
+<h1> Contactez-nous!</h1>
+
+
+<form method="post">
+  		<label for="email">Votre email</label>
+  		<br><input type="email" name="email" id="email" placeholder ="Ecrivez votre Email">
+
+  		<br><label for="textarea">Votre message</label>
+  		<br><textarea placeholder ="Ecrivez votre Message"></textarea>
+  		
+
+  		<br><input type="submit" value="Envoyer"> 
+</form>
+
+
+<?php include_once 'inc/footer.php'; ?>
