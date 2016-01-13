af21a8f68c54253dfd1f3f814121261763aa16bd
bertrand214 <zoubrouska@hotmail.com>
2016-01-13 11:57:22 +0100

    article

---
+++ article.php
@@ -0,0 +1,26 @@
+<?php include_once 'inc/header.php'; ?>
+
+<head>
+	<title>Article</title>
+</head>
+
+<body>
+	<main>
+		<h2>Titre de l'article</h2>
+
+		<div id="image">
+		<img src="#" alt="">
+		</div>
+
+		<div id="art">
+		<p>Ecrivez votre article</p>
+		</div>
+
+
+<form method="get">
+  		<label for="search">Votre recherche</label>
+  		<input type="text" name="search" id="search">
+  		<br><input type="submit" value="Envoyer"> 
+</form>
+
+<?php include_once 'inc/footer.php'; ?>
