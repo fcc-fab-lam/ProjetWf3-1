a94ff87a119bc6a8de9a07a345e30c09ea5ba9d3
bertrand214 <zoubrouska@hotmail.com>
2016-01-13 11:22:34 +0100

    index

---
+++ inc/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <title>Accueil</title>
+</head>
+<body>
+	<div class= "contenair">
+		<div class= "avatar">
+			<p>Prénom NOM</p>
+			<p> Tél :</p>
+			<p> Email:</p> 
+		</div>
+			<nav>
+				<ul> 
+					<li><a href="#ACCEUIL">ACCEUIL</a></li>
+					<li><a href="#NEWS">les news</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</nav>	
+	</div>
+
+<main>
+	<h1>Titre Porfolio</h1>
