96faee0a820b301b64f39a35c3c65ce2221d131c
lilithxxx <lilith.emy@gmail.com>
2016-01-15 15:39:37 +0100

    css + contact

--- contact.php
+++ contact.php
@@ -3,6 +3,12 @@
     $titrePage = 'Contact';
 	require_once  'inc/dbconnect.php';
 	include_once 'inc/header.php';
+
+	require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+
+	$mail = new PHPMailer;
+
+
 ?>
 
 <h1> Contactez-nous!</h1>
@@ -23,6 +29,9 @@ if(!empty($_POST)){
 	elseif(!filter_var($post['email'], FILTER_VALIDATE_EMAIL)){
 		$error[] = 'L\'adresse email est invalide';
 	}
+	if(empty($post['subject'])){
+		$error[] = 'Le sujet est obligatoire';
+	}
 
 	if(empty($post['message']))	{
 			$error[] ='le message est obligatoire'; 
@@ -32,11 +41,37 @@ if(!empty($_POST)){
 		$formError = true;
 	}
 	else {
-		$req = $bdd->prepare('INSERT INTO contact (email, message, new, date) VALUES(:email, :message, "yes", NOW())');
+		$req = $bdd->prepare('INSERT INTO contact (email, message, new, date, subject) VALUES(:email, :message, "yes", NOW(), :subject)');
 		$req->bindValue(':email', $post['email'], PDO::PARAM_STR);
+		$req->bindValue(':subject', $post['subject'], PDO::PARAM_STR);
 		$req->bindValue(':message', $post['message'], PDO::PARAM_STR);
 		if($req->execute()){
-			echo 'ok';
+				//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+			$mail->isSMTP();                                      // Set mailer to use SMTP
+			$mail->Host = 'smtp.mailgun.org';  // Specify main and backup SMTP servers
+			$mail->SMTPAuth = true;                               // Enable SMTP authentication
+			$mail->Username = 'equipe2@wf3.axw.ovh';                 // SMTP username
+			$mail->Password = 'BbPZm67u7d7iX9';                           // SMTP password
+			$mail->SMTPSecure = 'TLS';                            // Enable TLS encryption, `ssl` also accepted
+			$mail->Port = 587;                                    // TCP port to connect to
+
+			$mail->setFrom($post['email'], 'Mailer' );
+			$mail->addAddress('lilith.emy@gmail.com');               // Name is optional
+			$mail->addReplyTo('lilith.emy@gmail.com', 'Information');
+
+			$mail->isHTML(true);                                  // Set email format to HTML
+
+			$mail->Subject = $post['subject'];
+			$mail->Body    = $post['message'];
+			$mail->AltBody = $post['message'];
+
+			if(!$mail->send()) {
+			    echo 'Le mail n\'a pas été envoyé.';
+			    echo 'Mailer Error: ' . $mail->ErrorInfo;
+			} else {
+			    echo 'Le message a été envoyé';
+			}
 		}else{
 			echo 'nok';
 		}
@@ -52,6 +87,9 @@ if(!empty($_POST)){
 <form method="post">
   		<label for="email">Votre email</label>
   		<br><input type="email" name="email" id="email" placeholder ="Ecrivez votre Email">
+  		
+  		<br><label for="subject">Sujet de votre message</label>
+  		<br><textarea name="subject" placeholder ="Sujet..."></textarea>
 
   		<br><label for="textarea">Votre message</label>
   		<br><textarea name="message" placeholder ="Ecrivez votre Message"></textarea>
